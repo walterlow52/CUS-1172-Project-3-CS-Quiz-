42b7b605f0da9eda0b2ed1438e801ca9643153eb
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-16 10:47:12 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector("#project").onsubmit = function() {
     let users_name = document.querySelector('#name').value;
     alert(users_name);
-});
+};
 });
 
