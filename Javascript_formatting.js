ba5a3ba52bec7506b7aae72b5a4a7818700ea73c
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-18 19:47:40 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -3,6 +3,23 @@ document.addEventListener('DOMContentLoaded', function() {
     let first_name = document.querySelector('#fname').value;
     let last_name = document.querySelector('#lname').value;
     
+    function get_API_data () {
+      fetch('https://my-json-server.typicode.com/%3Cwalterlow52%3E/%3CCUS-1172-Project-3%3E').then(
+        (response) => {
+         return response.json();   
+        }
+        ).then(
+        (data) => {
+         updateDOM(data); 
+        }
+        
+        ).catch(
+          (err) => {
+           console.error(err); 
+          }
+        )
+    }
+    
     var template = Handlebars.template(templateSpec);
 };
 });
