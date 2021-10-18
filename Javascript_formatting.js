b0cc2cbdc54aab481ad6edae50134c6c3c00031a
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-18 19:38:21 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector("#project").onsubmit = function() {
     let first_name = document.querySelector('#fname').value;
     let last_name = document.querySelector('#lname').value;
+    
+    var template = Handlebars.template(templateSpec);
 };
 });
 
