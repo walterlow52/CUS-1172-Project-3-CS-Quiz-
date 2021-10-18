9dc86c508d3263e2259cef7dd87c3f0b1c60bc2d
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-18 19:51:41 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let last_name = document.querySelector('#lname').value;
     
     function get_API_data () {
-      fetch('https://my-json-server.typicode.com/%3Cwalterlow52%3E/%3CCUS-1172-Project-3%3E').then(
+      fetch('https://randomuser.me/api/').then(
         (response) => {
          return response.json();   
         }
