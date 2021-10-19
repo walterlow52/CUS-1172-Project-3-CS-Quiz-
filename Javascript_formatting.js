1ec8d45371e54a89dcc30a46017fa34e48934967
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-19 10:15:34 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector("#project").onsubmit = function() {
     let first_name = document.querySelector('#fname').value;
     let last_name = document.querySelector('#lname').value;
+  
+    var question1_Str = JSON.stringify(question_1);
+    console.log(question1_Str);
     
   /*document.addEventListener('click', function(event) {
   element = event.target;
