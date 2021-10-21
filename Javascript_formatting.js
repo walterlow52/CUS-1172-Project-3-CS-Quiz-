b6611107d050a4c4e75ad9110de50534d959a8f5
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-20 21:01:56 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector("#project").onsubmit = function() {
     let first_name = document.querySelector('#fname').value;
     let last_name = document.querySelector('#lname').value;
+    alert(first_name + last_name);
     
     var quiz_model = [
       {
