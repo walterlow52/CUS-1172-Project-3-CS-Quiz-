de00e9ba6e484b239d7dd4249011c90fd4acd4d4
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-24 20:18:59 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -9,11 +9,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector("#fetch").onsubmit = () => {
- async function async_fetch() {
-        const response = await fetch('https://randomuser.me/api/');
-        const data = await response.json();
-        updateDOM(data);
-      }
+    fetch('https://randomuser.me/api/').then(
+    (response) => {
+      return response.json()
+    }
+  ).then((results) => {
+      console.log(results)
+  }
+).catch( (err) => {
+  console.error(err);
+}
+)
       
     function updateDOM(data) {
      let image = data.results[0].picture.large;
