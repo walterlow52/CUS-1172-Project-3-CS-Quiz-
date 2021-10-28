2746f18250c2e348e4dd4a6e30bacc541daeeb9c
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-27 22:46:14 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -132,8 +132,8 @@ function update(quiz_application) {
   document.querySelector("#quiz_view").innerHTML = html_element;
 }
 
-const template_view = (model_, view_) => {
-  template_src = document.querySelector(view_).innerHTML
+const template_view = (model_, view) => {
+  template_src = document.querySelector(view).innerHTML
   var template = Handlebars.compile(template_src);
   var html_template = template({...model_, ...quiz_application})
   return html_template
