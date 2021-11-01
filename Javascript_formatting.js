cd13af4e50dd834c7d33ee52625ab00274e46169
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-11-01 15:56:10 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 const fetch_data = async () => {
   const data = await fetch("https://my-json-server.typicode.com/walterlow52/CUS-1172-Project-3/db")
   const model_ = await data.json()
-  const html_element = template_view(model_, '#view_quiz')
+  const html_element = template_view(model_, '#view_intro')
   document.querySelector("#fetchData").innerHTML = html_element;
 }
 
