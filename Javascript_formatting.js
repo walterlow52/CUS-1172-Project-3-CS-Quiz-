bd1477f9e9a88b23ff598b6b6ceb2730a2b68a2c
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-11-02 23:36:01 -0400

    Update Javascript_formatting.js

--- Javascript_formatting.js
+++ Javascript_formatting.js
@@ -38,10 +38,10 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
   function display(module) {
-    let quiz_text = module.questions[7].question;
-    let quiz_type = module.questions[7].type;
-    let quiz_answer = module.questions[7].answer;
-    let quiz_options = module.questions[7].choices;
+    let quiz_text = module.questions[17].question;
+    let quiz_type = module.questions[17].type;
+    let quiz_answer = module.questions[17].answer;
+    let quiz_options = module.questions[17].choices;
     let HTMLstring = `<h3> ${quiz_text} <br> </h3> <h4> Type: ${quiz_type} <br> ${quiz_answer} <br> ${quiz_options} </h4>`;
     document.querySelector("#showdata").innerHTML = HTMLstring;
   }
